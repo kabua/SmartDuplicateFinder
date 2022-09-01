fb5a887ba8067601c5d470dd0deaee29493f6fa3
Kabua <tristen.fielding@gmail.com>
2022-09-01 15:03:44 -0500

    A basic WPF application which can: - display unhandled exception - where a user can Exit from the application

---
+++ SmartDuplicateFinder/AppCommands.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SmartDuplicateFinder;
+
+public static class AppCommands
+{
+    private static readonly Type OwnerType = typeof(AppCommands);
+
+    public static readonly RoutedUICommand Exit = new("_Exit", "Exit", OwnerType, new InputGestureCollection(new[] { new KeyGesture(Key.F4, ModifierKeys.Alt) }));
+
+    public static readonly RoutedUICommand Okay = new("Ok", "Ok", OwnerType);
+    public static readonly RoutedUICommand Cancel = new("Cancel", "Cancel", OwnerType);
+    public static readonly RoutedUICommand Close = new("Close", "Close", OwnerType);
+}
